cb1f56c6dbb584cc1742a19e506a171367eb5ddb
thefutureseer <danielharrislimos@outlook.com>
2024-09-27 04:42:52 -0700

    add cross cors

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,7 +2,13 @@ import { ApolloServer } from 'apollo-server-micro';
 import typeDefs from '../../graphql/schema/index';
 import resolvers from '../../graphql/resolvers/index';
 import type { NextApiRequest, NextApiResponse } from 'next'; // Importing types for request and response
+import Cors from 'cors';
 
+const cors = Cors({
+  origin: 'https://soulbond-u4ynixe6j-thefutureseers-projects.vercel.app', // Specify the allowed origin
+  methods: ['POST', 'GET', 'OPTIONS'], // Specify allowed methods
+  allowedHeaders: ['Content-Type'], // Specify allowed headers
+})
 
 // Initialize the Apollo Server
 const apolloServer = new ApolloServer({
@@ -24,8 +30,19 @@ export const config = {
 // Start the server
 const startServer = apolloServer.start();
 
+// Middleware to run CORS
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: any) =>
+  new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) return reject(result);
+      resolve(result);
+    });
+  });
+
 // Export default async function to handle requests
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors); // Run the CORS middleware
+
   try {
     await startServer; // Ensure the server is started
     return apolloServer.createHandler({ path: '/api/graphql' })(req, res);

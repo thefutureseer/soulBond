7766f59447b590ce1ed3d93128bc363371742c81
thefutureseer <danielharrislimos@outlook.com>
2024-09-27 03:25:48 -0700

    move tailwind to dep

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,8 @@
 import { ApolloServer } from 'apollo-server-micro';
-import typeDefs from '../../graphql/schema';
-import resolvers from '../../graphql/resolvers';
+import typeDefs from '../../graphql/schema/index';
+import resolvers from '../../graphql/resolvers/index';
+import type { NextApiRequest, NextApiResponse } from 'next'; // Importing types for request and response
+
 
 // Initialize the Apollo Server
 const apolloServer = new ApolloServer({
@@ -23,7 +25,12 @@ export const config = {
 const startServer = apolloServer.start();
 
 // Export default async function to handle requests
-export default async function handler(req:any, res:any) {
-  await startServer; // Ensure the server is started
-  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await startServer; // Ensure the server is started
+    return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+  } catch (error) {
+    console.error('Error starting Apollo Server:', error);
+    res.status(500).send('Internal Server Error');
+  }
 }

545d85a692fb65d7cc7a83f9b69a20916c41d11c
thefutureseer <danielharrislimos@outlook.com>
2024-09-29 22:17:34 -0700

    simplify apollo server next no cors

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,59 +1,30 @@
-// pages/api/graphql.ts
 import { ApolloServer } from 'apollo-server-micro';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import typeDefs from '../../graphql/schema/index';
 import resolvers from '../../graphql/resolvers/index';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Context } from '../../app/types/context';
-import prisma from '../../app/utils/prisma';
+import prisma from '../../app/utils/prisma'; //Prisma here instead of resolvers
+import { Context } from '../../app/types/context'; 
 
-const allowedOrigins = [
-  'https://soulbond.vercel.app',
-  'http://localhost:3000', // development URL
-];
-
+// Create a new ApolloServer instance
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }): Context => {
-   return {prisma};
-  }
+  context: (): Context => ({ prisma }), //context, only passing Prisma
 });
 
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: false, // Required for `apollo-server-micro`
   },
 };
 
 const startServer = apolloServer.start();
 
+// Main handler function for the API route
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Start Apollo server once
   await startServer;
-
-  const origin = req.headers.origin;
-
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', origin && allowedOrigins.includes(origin) ? origin : '');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.end();
-    return;
-  }
-
-  // Set the Access-Control-Allow-Origin for all other requests
-  if (origin && typeof origin === 'string' && allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  } else {
-    res.setHeader('Access-Control-Allow-Origin', ''); // Deny access for other origins
-  }
-
-  try {
-    return apolloServer.createHandler({
-      path: '/api/graphql',
-    })(req, res);
-  } catch (error) {
-    console.error('Error handling request:', error);
-    res.status(500).send('Internal Server Error');
-  }
-};
+  
+  // Delegate request handling to Apollo Server
+  return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+};;

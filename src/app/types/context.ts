b4ce36a29235ec35a2244a3a0d25e212fed2bd20
thefutureseer <danielharrislimos@outlook.com>
2024-09-30 05:34:11 -0700

    generated prisma for soulpromise

--- src/app/types/context.ts
+++ src/app/types/context.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from '@prisma/client';
 
 export interface Context {
   prisma: PrismaClient;
-  user?: { id: number; email: string }; // Example user type if you have authentication
+ // user?: { id: number; email: string }; // Example user type if you have authentication
 }
--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-// Create a singleton instance of PrismaClient
 const prisma = new PrismaClient();
 
-// Export singleton instance
 export default prisma;

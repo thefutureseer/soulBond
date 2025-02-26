db76440bed4dc6be433ebac3c0e607c3b078bb83
thefutureseer <danielharrislimos@outlook.com>
2025-02-26 06:15:29 -0800

    update next config to include custom Webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Custom Webpack configuration
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;

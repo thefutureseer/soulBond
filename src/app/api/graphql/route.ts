d45e65184f5f07d66e60f022f6626f76d1043a78
thefutureseer <danielharrislimos@outlook.com>
2025-04-05 21:40:46 -0700

    add errors

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -7,23 +7,38 @@ import resolvers from 'graphql/resolvers';
 import typeDefs from 'graphql/schema';
 import prisma from 'src/utils/prisma';
 import { Context } from 'src/types/context';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "@apollo/server-plugin-landing-page-graphql-playground";
 
-// Define the GraphQL schema
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+// Utility function to create Apollo Server
+const Apollo_Server = () => {
 
-// Create Apollo Server with explicit type for Context
-const apolloServer = new ApolloServer<Context>({
-  schema,
-  persistedQueries: {
-    cache: new InMemoryLRUCache({ maxSize: 100 }), // Cache up to 100 queries
-  },
-  formatError: (error) => {
-    console.error(error);
-    return error;
-  },
-});
+  // Define the GraphQL schema
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+  // Create Apollo Server with explicit type for Context
+  return new ApolloServer<Context>({
+    schema,
+    persistedQueries: {
+      cache: new InMemoryLRUCache({ maxSize: 100 }), // Cache up to 100 queries
+    },
+    formatError: (error) => {
+      console.error(`[GraphQL Error]: ${error.message}`);
+      if (error.extensions?.exception) {
+        console.error(`[Exception: GraphQL Rout.ts error: ]:`, error.extensions.exception);
+      }
+      return {
+        message: error.message,
+        locations: error.locations,
+        path: error.path,
+      };
+    },
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+};
 // Create Apollo handler
+const apolloServer = Apollo_Server();
 const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (): Promise<Context> => ({ prisma }),
 });
